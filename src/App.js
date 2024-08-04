3b95775ba15fc26b1b079373d789d8877f6a1545
Himanshu Bajpai <himanshu.bajpai@mobileprogramming.com>
2024-08-04 22:11:15 +0530

    Added mode functionality through App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import {useState} from 'react'
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = ()=>{
+    if(mode === 'dark'){
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+    }else{
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743'
+    }
+      
+    
+  }
   return (
     <>
-    <Navbar title="TextUtils"/>
+    <Navbar title="TextUtils" mode ={mode} toggleMode ={toggleMode}/>
     <div className="container my-3">
-      <TextForm heading="Enter the text to analyse below"/>
+      <TextForm heading="Enter the text to analyse below" mode ={mode} />
+      {/* <About /> */}
     </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -24,16 +24,16 @@ export default function TextForm(props) {
 
     return (
         <>
-        <div className='container'>
+        <div className='container' style={{color: props.mode == 'dark'? 'white' : 'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text}  id="myBox" rows="8" onChange={handleOnChange}></textarea>
+                <textarea className="form-control" value={text} style={{backgroundColor:props.mode == 'dark'? 'grey' : 'white', color: props.mode=='dark'? 'white': 'black'}}  id="myBox" rows="8" onChange={handleOnChange}></textarea>
             </div>
             <button className = "btn btn-primary mx-1" onClick= {handleUpClick}>Convert to Uppercase</button>
             <button className = "btn btn-primary mx-1" onClick= {handleLoClick}>Convert to Lowercase</button>
             <button className = "btn btn-primary mx-1" onClick= {handleClearText}>Clear Text</button>
         </div>
-        <div className='container my-3'> 
+        <div className='container my-3' style={{color: props.mode == 'dark'? 'white' : 'black'}}> 
             <h2>Text Summary</h2>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} minutes to read this sentence</p>
